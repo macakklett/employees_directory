8b918a4281b17aebe77dc1e7eaa480be90a5a043
macakklett <macanvitalijj@gmail.com>
2024-09-20 10:36:38 +0300

    fix

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const CopyPlugin = require('copy-webpack-plugin');
 const webpack = require('webpack');
 const path = require('path');
 
@@ -54,6 +55,9 @@ module.exports = (_, argv) => {
       new CleanWebpackPlugin(),
       new HtmlWebpackPlugin({
         template: './src/index.html',
+      }),
+      new CopyPlugin({
+        patterns: [{ from: 'public/_redirects', to: 'dist' }],
       }),
     ],
     devServer: {
